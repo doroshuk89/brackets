7476bf1e2c069e3e25ab3850439a4cf7cfb1aff4
Altas dallas <atlas.webdev89@gmail.com>
2022-07-20 13:27:22 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // Преобразуем строку в массив
+  const arrStr = str.split('');
+  // Преобразуем в одномерный массив
+  const brackets = bracketsConfig.reduce((acc, value) => {
+        acc.push(...value);
+    return acc;
+  }, [])
+
+  // Добавляем стэк
+  const stack = [];
+  
+  // Функция нахождения индекса элемента в bracketsConfig
+  const indexBrackets = (element) => brackets.indexOf(element);
+  
+    for (let i = 0; i < arrStr.length; i++) {
+      let indexElem =  indexBrackets (arrStr[i]);
+      if(indexElem % 2 === 0) {
+          if(arrStr[i] === brackets[indexElem +1]  && stack[stack.length-1] === arrStr[i]) {
+            stack.pop();
+          }else if (arrStr[i] === brackets[indexElem + 1] && stack[stack.length-1] !== arrStr[i]) {
+            stack.push(arrStr[i]);
+          }else {
+            stack.push(arrStr[i]);
+          }
+      }else {
+        if (stack.pop() !== brackets[indexElem - 1]) {
+            return false;
+        }
+      }
+    }
+  return stack.length === 0;
 }
